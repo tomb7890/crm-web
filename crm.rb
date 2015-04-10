43b2af4ef5f27b554fb331a8b17dd21c9393bcc8
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-10 14:56:59 -0400

    raise Sinatra::NotFound

--- crm.rb
+++ crm.rb
@@ -28,7 +28,11 @@ end
 
 get "/contacts/:id" do
   @contact = $rolodex.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+     raise Sinatra::NotFound
+  end
 end
 
 # as the last route, add
