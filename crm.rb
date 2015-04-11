ceb72727a6fad4a94d62ac029cffe50f9111340a
Tom Billingsley <tom.billingsley@gmail.com>
2015-04-11 10:25:43 -0400

    initial work on edit contact

--- crm.rb
+++ crm.rb
@@ -35,6 +35,15 @@ get "/contacts/:id" do
   end
 end
 
+get "/contacts/:id/edit" do
+ @contact = $rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 # as the last route, add
 get '/contacts/new' do
   erb :new_contact
